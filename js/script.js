8c668f36957e8f4ebaf0afe312d59ec1c0c08d79
alexa <aleksandra12.07.10@gmail.com>
2014-07-29 16:17:11 +0400

    [+] layout

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function(){
+    if ($.fn.bjqs) {
+        $('#slider').bjqs({
+            'height': 467,
+            'width': 940,
+            'display': 'inline-block',
+            'position': 'relative',
+            'responsive': false,
+            'automatic': true,
+            'animtype': 'fade'
+        });
+    }
+});
